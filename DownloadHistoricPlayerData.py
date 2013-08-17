289680165b6eead80a4f69c62dff3956f0c746e8
bpafoshizle <bpafoshizle@gmail.com>
2013-08-16 20:34:12 -0500

    Working up to each players initial Gamelog page

--- DownloadHistoricPlayerData.py
+++ DownloadHistoricPlayerData.py
@@ -8,13 +8,39 @@ baseYahooSportsUrl = 'http://sports.yahoo.com'
 yahooSportsNFLUrl = baseYahooSportsUrl + '/nfl/'
 
 
-
 def main(argv):
 	# Get Stats Page to get all the Positions
 	# Drill in to every position
 	
-	getPositionPlayerListingPage()
+	getPlayerGameLogData()
 	
+
+def getPlayerGameLogData():
+	""" Gets a table format of player game log data for
+		every position available on sports.yahoo.com
+	"""
+
+	positionUrlList = getPositionUrlsFromYahooStatsPage()
+	positionPages = getPositionPlayerListingPages(positionUrlList)
+	playerUrls = getPlayerUrlsFromPositionPages(positionPages)
+
+
+def getPlayerUrlsFromPositionPages(positionPages):
+	""" Function to extract and build all the URLs for
+		every player link on a position page
+	"""
+	playerDictList = []
+	for key, value in positionPages.iteritems():
+		playerPaths = re.finditer(r'<a href="(/nfl/players/\d+)">(.*?)</a>', value)
+		for playerPath in playerPaths:
+			playerDictList.append({'playerUrl':baseYahooSportsUrl + playerPath.group(1) + '/gamelog', 'playerName':playerPath.group(2), 'playerPos':key})
+		
+	
+	print playerDictList
+
+
+
+
 def getPositionUrlsFromYahooStatsPage():
 	""" Get the initial Stats Page that will allow us to 
 		bootstrap this whole process by digging in to the 
@@ -33,14 +59,22 @@ def getPositionUrlsFromYahooStatsPage():
 
 
 	
-def getPositionPlayerListingPages():
-	for positionUrl in getPositionUrlsFromYahooStatsPage():
+def getPositionPlayerListingPages(positionUrlList):
+	""" Method to create a dictionary of position pages 
+		addressable by position name, e.g. QB, RB, TE, C, 
+		etc, and derived from the main yahoo stats page
+	"""
+	positionPages = {}
+	for positionUrl in positionUrlList:
 		positionName = extractPositionNameFromUrl(positionUrl)
 		response = urllib2.urlopen(positionUrl)
 		pageHtml = response.read()
-		positionPageFile = open('C:\Users\MILLERBARR\Documents\GitHub\FantasyFootballStats\\\\' + positionName + 'Page.html', 'w')
-		positionPageFile.write(pageHtml)
-		positionPageFile.close()
+		positionPages[positionName] = pageHtml
+		#positionPageFile = open('C:\Users\MILLERBARR\Documents\GitHub\FantasyFootballStats\\\\' + positionName + 'Page.html', 'w')
+		#positionPageFile.write(pageHtml)
+		#positionPageFile.close()
+
+	return positionPages
 	
 
 def extractPositionNameFromUrl(positionUrl):
