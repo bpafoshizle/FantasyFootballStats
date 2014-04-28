160526fec8ecac3b16f8efbba7e60d40eb2e4c5d
bpafoshizle <bpafoshizle@gmail.com>
2014-04-27 20:44:50 -0500

    Started extractor code

--- Extractor/ProFootballRefExtractor.py
+++ Extractor/ProFootballRefExtractor.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup
+from os import listdir
+from os.path import isfile, join
 
 
 class ProFootballRefExtractor:
@@ -7,8 +9,10 @@ class ProFootballRefExtractor:
 	CSV files, which can be used as clean data sources to load 
 	a database. """
 
-	def __init__(self, dataDir='./Data/Raw/ProFootballRef/'):
-		self.dataDir = dataDir
+	def __init__(self, sourceDataDir='./Data/Raw/ProFootballRef/'):
+		self.sourceDataDir = sourceDataDir
+		self.sourceGameLogDir = join(sourceDataDir, 'playerGameLogPages')
+		self.sourcePlayerDir = join(sourceDataDir, 'playerPages')
 		
 	def extractPlayerData(self):
 		""" Method that will extract player data from player pages"""
@@ -16,3 +20,24 @@ class ProFootballRefExtractor:
 	
 	def extractPlayerGameLogData(self):
 		""" Method to extract game log data from player game log pages""" 
+		htmlFiles = self.getHtmlFiles()
+		for htmlFile in htmlFiles:
+			soup = BeautifulSoup(open(htmlFile))
+			regSeasStatsTab, poStatsTab = self.getStatsTables(soup)	
+			regSeasStatsForm = self.getTabFormat(regSeasStatsTab)
+			poStatsForm = self.getTabFormat(poStatsTab)
+
+			print(regSeasStatsForm)
+			print(poStatsForm)
+
+	
+	def getStatsTables(self, soup):
+		regSeasStatsTab = soup.find('table', id='stats')
+		poStatsTab = soup.find('table', id='stats_playoffs')
+		return regSeasStatsTab, poStatsTab
+
+	def getTabFormat(self, tab):
+		return tab.thead.find_all('th', attrs={'data-stat': True})
+
+	def getHtmlFiles(self):
+		return [join(self.sourceGameLogDir, f) for f in listdir(self.sourceGameLogDir) if isfile(join(self.sourceGameLogDir, f))]
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 from WebBot.ProFootballRefBot import ProFootballRefBot
+from Extractor.ProFootballRefExtractor import ProFootballRefExtractor
 
 
 def main(argv):
-	bot = ProFootballRefBot()
-	bot.downloadGamelogPagesForYear('2013')
+	#bot = ProFootballRefBot()
+	#bot.downloadGamelogPagesForYear('2013')
+	extractor = ProFootballRefExtractor(sourceDataDir='./Data/Raw/ProFootballRef/Testing')
+	extractor.extractPlayerGameLogData()
 
 	
 if(__name__ == '__main__'):
